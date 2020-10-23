95abc099e05b6df04c76f2e201678e1885afebd8
Michael Ellis <michael94ellis@gmail.com>
2020-10-23 12:49:58 -0400

    Initial Commit

---
+++ QRCoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRCoder
+//
+//  Created by Michael Ellis on 10/23/20.
+//  Copyright © 2020 Michael Ellis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
